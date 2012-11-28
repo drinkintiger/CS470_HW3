d7eade1727c2a8afdfdb02cbb5abba33ac4099e8
drinkintiger <tsoy_vladimir@yahoo.com>
2012-11-27 20:07:16 -0600

    merging fork

--- preorder.c
+++ preorder.c
@@ -1,27 +1,41 @@
-BuildTree(argv[1], &root, &nodes, &count,&leftMost);
+/*
+ * Vladimir Tsoy & Jordan Harmel
+ * Nov. 11, 2012
+ *
+ * CS 470 : Project 3
+ */
+#include <pthread.h>
+#include "tree.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-#pragma omp parallel default(shared) private(i) num_threads(count)
-{
-    i = omp_get_thread_num();
-    if (nodes[i]->sibling == NULL)
-    nodes[i]->order = nodes[i]->parent;
-}
+int main(int argc, char * argv[]) {
 
-#pragma omp parallel default(shared) private(i, j, n, d, temp) num_threads(count)
-{
-    i = omp_get_thread_num();
-    temp = nodes[i];
-    for (j = 1; j <= count; j = 2*j) {
-        if (temp->order != NULL ){
-            n = temp->order->order;
-            d = temp->post + temp->order->post;
+    BuildTree(argv[1], &root, &nodes, &count,&leftMost);
+
+    #pragma omp parallel default(shared) private(i) num_threads(count)
+    {
+        i = omp_get_thread_num();
+        if (nodes[i]->sibling == NULL)
+        nodes[i]->order = nodes[i]->parent;
+    }
+
+    #pragma omp parallel default(shared) private(i, j, n, d, temp) num_threads(count)
+    {
+        i = omp_get_thread_num();
+        temp = nodes[i];
+        for (j = 1; j <= count; j = 2*j) {
+            if (temp->order != NULL ){
+                n = temp->order->order;
+                d = temp->post + temp->order->post;
+            }
+        #pragma omp barrier
+        if (temp->order != NULL) {
+            temp->post = d;
+            temp->order = n;
         }
-    #pragma omp barrier
-    if (temp->order != NULL) {
-        temp->post = d;
-        temp->order = n;
+        #pragma omp barrier
+        }
+        temp->post = count - temp->post;
     }
-    #pragma omp barrier
-    }
-    temp->post = count - temp->post;
 }
