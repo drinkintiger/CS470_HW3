dc1d3e30b33c5d3d0eaba17b816e4c45af7a6d53
drinkintiger <tsoy_vladimir@yahoo.com>
2012-11-30 13:09:14 -0600

    complete rework

--- preorder.c
+++ preorder.c
@@ -5,37 +5,112 @@
  * CS 470 : Project 3
  */
 #include <pthread.h>
-#include "tree.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+char *name;
+struct Node *parent;
+struct Node *left;
+struct Node *right;
+struct Node *sibling;
+struct Node *order;
+int post;
+}Node;
+
+char *substring(char * input, int length);
+void split(char * in, char ** first, char ** rest);
+Node *buildTree (Node * parent, char * node, char * sibling);
 
 int main(int argc, char * argv[]) {
+    char * first;
+    char * rest;
+    Node *temp = malloc(sizeof(Node));
+    Node *parent = malloc(sizeof(Node));
+    
+    temp = buildTree(parent, "(A(B)(C(E)(F))(D(G)))", NULL);
+    //split ("(A)(B)(C)", &first, &rest);
+    //printf ("First : %s Rest: %s\n", first, rest);
+}
 
-    BuildTree(argv[1], &root, &nodes, &count,&leftMost);
-
-    #pragma omp parallel default(shared) private(i) num_threads(count)
-    {
-        i = omp_get_thread_num();
-        if (nodes[i]->sibling == NULL)
-        nodes[i]->order = nodes[i]->parent;
-    }
-
-    #pragma omp parallel default(shared) private(i, j, n, d, temp) num_threads(count)
-    {
-        i = omp_get_thread_num();
-        temp = nodes[i];
-        for (j = 1; j <= count; j = 2*j) {
-            if (temp->order != NULL ){
-                n = temp->order->order;
-                d = temp->post + temp->order->post;
+Node *buildTree (Node * parent, char * node, char * sibling) {
+    int start = -1, end, length, next, count = 0;
+    char * child;
+    char * temp;
+    char * first;
+    char * rest;
+    
+    length = strlen(node);
+    
+    for (int i = 0; i < length; ++i) {
+        if (node[i] == '(') {
+            if (start < 0) {
+                start = i;
+                count += 1;
             }
-        #pragma omp barrier
-        if (temp->order != NULL) {
-            temp->post = d;
-            temp->order = n;
+            else if (count == 1) {
+                parent -> name = substring(&node[start], i - 1);
+                temp = substring(&node[i], length - i + 1);
+                split(temp, &first, &rest);
+                printf ("The remainder is : %s\n", temp);
+                count += 1;
+            }
         }
-        #pragma omp barrier
+        else if (node[i] == ')') {
+            count -= 1;
+            if (count == 0) {
+                end = i;
+                break;
+            }
         }
-        temp->post = count - temp->post;
+    }
+    return parent;
+}
+char *substring(char * input, int length) {
+    char * temp = malloc(sizeof(char) * (length + 1));
+    temp = memcpy(temp, input, length);
+    temp[length] = 0;
+    return temp;
+}
+
+void split(char * in, char ** first, char ** rest) {
+    int length = strlen(in);
+    int start = -1, count = 0, end;
+    
+    if (in == NULL || in == '\0') {
+        *first = NULL;
+        *rest = NULL;
+        return;
+    }
+    
+    for (int i = 0; i < length; ++i) {
+        if (in[i] == '(') {
+            if (start == -1) {
+                start = i;
+            }
+            count += 1;
+        }
+        else if (in[i] == ')') {
+            count -= 1;
+            if (count == 0) {
+                end = i;
+                break;
+            }
+        }
+    }
+    
+    if (start < 0) {
+        *first = NULL;
+        *rest = NULL;
+        return;
+    }
+    
+    *first = substring (&in[start], end - start + 1);
+    if (end + 1 >= length) {
+        *rest = NULL;
+    }
+    else {
+        *rest = substring(&in[end + 1], length - (end +1));
     }
 }
