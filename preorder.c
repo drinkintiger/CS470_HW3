1d38fa21609916a54dfa23a79244bfd59ea8521a
drinkintiger <tsoy_vladimir@yahoo.com>
2012-11-30 20:20:18 -0600

    slowly but surely

--- preorder.c
+++ preorder.c
@@ -20,7 +20,7 @@ int pre;
 
 char *substring(char * input, int length);
 void split(char * in, char ** first, char ** rest);
-Node *buildTree (Node * parent, char * node, char * sibling);
+Node *buildNode (Node * parent, char * node, char * sibling);
 
 int main(int argc, char * argv[]) {
     char * first;
@@ -28,26 +28,26 @@ int main(int argc, char * argv[]) {
     Node *temp = malloc(sizeof(Node));
     Node *parent = malloc(sizeof(Node));
     
-    temp = buildTree(parent, "(A(B)(C(E)(F))(D(G)))", NULL);
+    temp = buildNode(parent, "(A(B)(C(E)(F))(D(G)))", NULL);
     //split ("(A)(B)(C)", &first, &rest);
     //printf ("First : %s Rest: %s\n", first, rest);
 }
 
-Node *buildTree (Node * parent, char * node, char * sibling) {
+Node *buildNode (Node * parent, char * node, char * sibling) {
     int start = -1, end, length, next, count = 0;
-    char * child;
+    Node * child;
+    Node * sibling;
     char * temp;
     char * first;
     char * rest;
-    Node * temp1 = malloc(sizeof(Node));
-    
+
     length = strlen(node);
-    
     for (int i = 0; i < length; ++i) {
         if (node[i] == '(') {
             if (start < 0) {
                 start = i;
                 count += 1;
+                next = i;
             }
             else if (count == 1) {
                 if (parent->name == NULL) {
@@ -60,20 +60,23 @@ Node *buildTree (Node * parent, char * node, char * sibling) {
         else if (node[i] == ')') {
             count -= 1;
             if (count == 0) {
-                temp = substring(&node[next], length - (next + 1));
+                temp = substring(&node[start + 1], length - 2 );
                 end = i;
                 break;
             }
         }
     }
-    
-    printf("%s\n", temp);
+
     split(temp, &first, &rest);
-    buildTree(temp1, temp, NULL);
+    
     printf ("The parent is : %s\n", parent -> name);
     printf ("The child is : %s\n", first);
     printf ("The remainder is : %s\n", rest);
-
+    
+    if (first == NULL && rest == NULL) {
+        return parent;
+    }
+    parent->child = buildNode(parent, first, rest);
     return parent;
 }
 char *substring(char * input, int length) {
